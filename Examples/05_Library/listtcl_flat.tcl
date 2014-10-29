39a7c42d355496ddefefd6455123e68d06affaf0
Martin Weitzel <martin@tbfe.de>
2014-10-29 14:32:52 +0100

    Added solution for step 2 of exercise Tcl-File-Lister

--- Examples/05_Library/listtcl_flat.tcl
+++ Examples/05_Library/listtcl_flat.tcl
@@ -4,18 +4,24 @@
 # List all Tcl-Files in current directory
 # ================================================================
 
-foreach fname [glob *.tcl] {
-    puts [string repeat "/" [expr [string length $fname] + 8]]
-    puts "| File: $fname"
-    puts [string repeat "\\" [expr [string length $fname] + 8]]
-    if {[catch {open $fname} chan]} {
-        puts "ERROR: $chan
-        continue
+proc list_tcl {dir} {
+    foreach fname [glob -directory $dir *.tcl] {
+        puts [string repeat "/" [expr [string length $fname] + 8]]
+        puts "| File: $fname"
+        puts [string repeat "\\" [expr [string length $fname] + 8]]
+        if {[catch {open $fname} chan]} {
+            puts "ERROR: $chan
+            continue
+        }
+        set nr 0
+        while {[gets $chan line] >= 0} {
+            puts [format {%6d: %s} [incr nr] $line]
+        }
+        close $chan
+        puts ""
     }
-    set nr 0
-    while {[gets $chan line] >= 0} {
-        puts [format {%6d: %s} [incr nr] $line]
-    }
-    close $chan
-    puts ""
 }
+
+if {$argc == 0}\
+then {list_tcl .}\
+else {foreach d $argv {list_tcl $d}}
