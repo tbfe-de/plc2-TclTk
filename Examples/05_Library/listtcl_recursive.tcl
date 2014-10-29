72014c6b742dd72fedf1f14d187f846b96fdc35e
Martin Weitzel <martin@tbfe.de>
2014-10-29 14:33:58 +0100

    Added solution for step 3 of exercise Tcl-File-Lister

---
+++ Examples/05_Library/listtcl_recursive.tcl
@@ -0,0 +1,30 @@
+#!/usr/bin/tclsh
+
+# ================================================================
+# List all Tcl-Files in current directory
+# ================================================================
+
+proc list_tcl {dir} {
+    foreach fname [glob -nocomplain -directory $dir *.tcl] {
+        puts [string repeat "/" [expr [string length $fname] + 8]]
+        puts "| File: $fname"
+        puts [string repeat "\\" [expr [string length $fname] + 8]]
+        if {[catch {open $fname} chan]} {
+            puts "ERROR: $chan
+            continue
+        }
+        set nr 0
+        while {[gets $chan line] >= 0} {
+            puts [format {%6d: %s} [incr nr] $line]
+        }
+        close $chan
+        puts ""
+    }
+    foreach subdir [glob -nocomplain -directory $dir -type d *] {
+        list_tcl $subdir
+    }
+}
+
+if {$argc == 0}\
+then {list_tcl .}\
+else {foreach d $argv {list_tcl $d}}
